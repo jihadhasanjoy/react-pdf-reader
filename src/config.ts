abedddab294e3bf5b65832bcf457cd434b637e31
Joybs23 <joy@brain@0147852>
2022-06-29 19:52:10 +0600

    remvoe: extra base file path

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,9 @@
 
-// local Url
-
 const baseFilePath = 'resources/';
-
-// gitHubUrl
-
-//const baseFilePath = 'https://jihadhasanjoy.github.io/test-site-publish/resources/'
-
-
 export const pdfListDataUrl = `${baseFilePath}pdf-file.json`;
 export const pdfImageUrl = `${baseFilePath}pdf.png`
 export const workerUrl = `${baseFilePath}pdf.worker.min.js`;
 export const pdfFilePath = baseFilePath;
+
+
+// 'https://jihadhasanjoy.github.io/test-site-publish/resources/'
